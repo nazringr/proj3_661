e6b6c7b83bac218aaf21e98b4751fcb76d34c204
ONKAR KHER <okher@umd.edu>
2024-03-24 23:04:35 -0400

    Updated some comments to make code more readable

--- a_star.py
+++ a_star.py
@@ -10,8 +10,7 @@ import os
 import cv2
 
 # Initializing the class for our "Node" objects
-# Initializing the class for our "Node" objects
-class Node:
+class SearchNode:
     def __init__(self, x, y, orient, cost, parent_id, cost_to_go = 0):
         self.x = x
         self.y = y
@@ -25,106 +24,104 @@ class Node:
         return self.cost + self.cost_to_go < other.cost + other.cost_to_go
     
 # Defining the action set, and calculating the costs
-def move_60up(x,y,orient,step_size, cost):
+def move_60up(x,y,orient,step_s, cost):
     orient = orient + 60
-    x = x + (step_size*np.cos(np.radians(orient)))
-    y = y + (step_size*np.sin(np.radians(orient)))
+    x = x + (step_s*np.cos(np.radians(orient)))
+    y = y + (step_s*np.sin(np.radians(orient)))
     x = round(x)
     y = round(y)
     cost = 1 + cost
     return x,y,orient,cost
 
-def move_60down(x,y,orient, step_size, cost):
+def move_60down(x,y,orient, step_s, cost):
     orient = orient - 60
-    x = x + (step_size*np.cos(np.radians(orient)))
-    y = y + (step_size*np.sin(np.radians(orient)))
+    x = x + (step_s*np.cos(np.radians(orient)))
+    y = y + (step_s*np.sin(np.radians(orient)))
     x = round(x)
     y = round(y)
     cost = 1 + cost
     return x,y,orient,cost
 
-def move_30up(x,y,orient, step_size, cost):
+def move_30up(x,y,orient, step_s, cost):
     orient = orient + 60
-    x = x + (step_size*np.cos(np.radians(orient)))
-    y = y + (step_size*np.sin(np.radians(orient)))
+    x = x + (step_s*np.cos(np.radians(orient)))
+    y = y + (step_s*np.sin(np.radians(orient)))
     x = round(x)
     y = round(y)
     cost = 1 + cost
     return x,y,orient, cost
 
-def move_30down(x,y,orient, step_size, cost):
+def move_30down(x,y,orient, step_s, cost):
     orient = orient - 30
-    x = x + (step_size*np.cos(np.radians(orient)))
-    y = y + (step_size*np.sin(np.radians(orient)))
+    x = x + (step_s*np.cos(np.radians(orient)))
+    y = y + (step_s*np.sin(np.radians(orient)))
     x = round(x)
     y = round(y)
     cost = 1 + cost
     return x,y,orient, cost
 
-def move_straight(x,y,orient, step_size, cost):
+def move_straight(x,y,orient, step_s, cost):
     orient = orient + 0
-    x = x + (step_size*np.cos(np.radians(orient)))
-    y = y + (step_size*np.sin(np.radians(orient)))
+    x = x + (step_s*np.cos(np.radians(orient)))
+    y = y + (step_s*np.sin(np.radians(orient)))
     x = round(x)
     y = round(y)
     cost = 1 + cost
     return x,y,orient, cost
 
 # Initializing the action set functions
-def Action_set(move,x,y,orient,step_size,cost):
+def Actions(move,x,y,orient,step_s,cost):
 
-    if move == 'extreme_right':
-        return move_60up(x,y,orient, step_size,cost)
+    if move == '60_up':
+        return move_60up(x,y,orient, step_s,cost)
     elif move == 'right':
-        return move_30up(x,y,orient, step_size,cost)
+        return move_30up(x,y,orient, step_s,cost)
     elif move == 'straight':
-        return move_straight(x,y,orient,step_size,cost)
+        return move_straight(x,y,orient,step_s,cost)
     elif move == 'left':
-        return move_30down(x,y,orient,step_size,cost)
-    elif move == 'extreme_left':
-        return move_60down(x,y,orient,step_size,cost)
+        return move_30down(x,y,orient,step_s,cost)
+    elif move == '60_down':
+        return move_60down(x,y,orient,step_s,cost)
     else:
         return None
 
 #Initializing the canvas with the obstacle definitions
-def obstacle_space(width, height, total_clearance, robot_radius):
-
-    obs_space_array = np.full((height,width),0)
+def canvas_space(w, h, total_clearance, r_radius):
+    obs_space_array = np.full((h,w),0)
     
-    for y in range(height) :
-        for x in range(width):
+    for y in range(h) :
+        for x in range(w):
         
             # Boundary total_clearance with buffer
-            if x<total_clearance+robot_radius or x>width-(total_clearance+robot_radius) or y<total_clearance+robot_radius or y>height-(total_clearance+robot_radius):
+            if x<total_clearance+r_radius or x>w-(total_clearance+r_radius) or y<total_clearance+r_radius or y>h-(total_clearance+r_radius):
                 obs_space_array[y,x] = 1  # Filled as obstacle
             # Boundary total_clearance with buffer
-            if x<total_clearance+robot_radius or x>width-(total_clearance+robot_radius) or y<total_clearance+robot_radius or y>height-(total_clearance+robot_radius):
+            if x<total_clearance+r_radius or x>w-(total_clearance+r_radius) or y<total_clearance+r_radius or y>h-(total_clearance+r_radius):
                 obs_space_array[y,x] = 1  # Filled as obstacle
 
             # Rightmost mirrored C shaped obstacle
-            if x>900-total_clearance+robot_radius and x<1100+total_clearance+robot_radius and y>50-(total_clearance+robot_radius) and y<125+total_clearance+robot_radius:
+            if x>900-total_clearance+r_radius and x<1100+total_clearance+r_radius and y>50-(total_clearance+r_radius) and y<125+total_clearance+r_radius:
                 obs_space_array[y,x] = 1
-            if x>1020-(total_clearance+robot_radius) and x<1100+total_clearance+robot_radius and y>125-(total_clearance+robot_radius) and y<375+total_clearance+robot_radius:
+            if x>1020-(total_clearance+r_radius) and x<1100+total_clearance+r_radius and y>125-(total_clearance+r_radius) and y<375+total_clearance+r_radius:
                 obs_space_array[y,x] = 1      
-            if x>900-(total_clearance+robot_radius) and x<1100+total_clearance+robot_radius and y>375-(total_clearance+robot_radius) and y<450+total_clearance+robot_radius:
+            if x>900-(total_clearance+r_radius) and x<1100+total_clearance+r_radius and y>375-(total_clearance+r_radius) and y<450+total_clearance+r_radius:
                 obs_space_array[y,x] = 1  
 
             # Left rectangle obstacle
-            if x>100-(total_clearance+robot_radius) and x<175+total_clearance+robot_radius and y>100-(total_clearance+robot_radius):
+            if x>100-(total_clearance+r_radius) and x<175+total_clearance+r_radius and y>100-(total_clearance+r_radius):
                 obs_space_array[y,x] = 1
 
             # Right rectangle obstacle
-            if x>275-(total_clearance+robot_radius) and x<350+total_clearance+robot_radius and y<400+total_clearance+robot_radius:
+            if x>275-(total_clearance+r_radius) and x<350+total_clearance+r_radius and y<400+total_clearance+r_radius:
                 obs_space_array[y,x] = 1
 
             # Hexagonal obstacle
-            # Appropriate total_clearance buffer has been added to the  
-            # intercept values for simpler implementation
-            if x<785+(total_clearance+robot_radius) and x>515-(total_clearance+robot_radius):
-                if 1.73*y+x-(1342.25+2*(total_clearance+robot_radius))<0:
-                    if 1.73*y+x-(823-2*(total_clearance+robot_radius))>0:
-                        if 1.73*y-x-(42+2*(total_clearance+robot_radius))<0:
-                            if 1.73*y-x+477.25+2*(total_clearance+robot_radius)>0:
+            # Appropriate total_clearance buffer has been added to the intercept values for simpler implementation 
+            if x<785+(total_clearance+r_radius) and x>515-(total_clearance+r_radius):
+                if 1.73*y+x-(1342.25+2*(total_clearance+r_radius))<0:
+                    if 1.73*y+x-(823-2*(total_clearance+r_radius))>0:
+                        if 1.73*y-x-(42+2*(total_clearance+r_radius))<0:
+                            if 1.73*y-x+477.25+2*(total_clearance+r_radius)>0:
                                 obs_space_array[y,x] = 1              
     return obs_space_array
     
@@ -165,65 +162,56 @@ def valid_orientation(orient):
         return False
 
 # A* Algorithm
-def a_star_algorithm(start,goal,obs_space_array,step_size):                       
-
+def a_star_algorithm(start,goal,obs_space_array,step_s):                       
     if goal_reached(start, goal):
         return None,1
     goal_node = goal
     start_node = start
     
-    available_moves = ['extreme_right','right', 'straight', 'left', 'extreme_left']   
-    unexplored_nodes = {}  # List of all open nodes
+    available_moves = ['60_up','right', 'straight', 'left', '60_down']   
+    new_nodes = {}  # Creating a list of all the open nodes
     start_key = key(start_node) # New key which points to the new node
-    unexplored_nodes[(start_key)] = start_node
-    nodes_explored = {} #List of all the closed nodes
-    priority_list = []  #List to store all dictionary entries with cost as the sorting variable
-    heapq.heappush(priority_list, [start_node.cost, start_node]) # Least cost entries will be prioritized
+    new_nodes[(start_key)] = start_node
+    old_nodes = {} #Creating a list of all the closed nodes
+    p_list = []  # Creating a list to store all dictionary entries with cost as the sorting variable
+    heapq.heappush(p_list, [start_node.cost, start_node]) # Least cost entries will be prioritized
     all_nodes = [] #storing all the nodes that have been traversed, for visualization purposes.
-
-    while (len(priority_list) != 0):
-
-        current_node = (heapq.heappop(priority_list))[1]          
+    while (len(p_list) != 0):
+        current_node = (heapq.heappop(p_list))[1]          
         all_nodes.append([current_node.x, current_node.y, current_node.orient])          
         current_id = key(current_node)
-
         # If the goal is reached
         if goal_reached(current_node, goal_node):
             goal_node.parent_id = current_node.parent_id
             goal_node.cost = current_node.cost
             print("Found the GOAL!")
             return all_nodes,1
-
-        if current_id in nodes_explored:  
+        if current_id in old_nodes:  
             continue
         else:
-            nodes_explored[current_id] = current_node
-        del unexplored_nodes[current_id]
-
+            old_nodes[current_id] = current_node
+        del new_nodes[current_id]
         for move in available_moves:
-            x,y,orient,cost = Action_set(move,current_node.x,current_node.y,current_node.orient, step_size, current_node.cost)  
+            x,y,orient,cost = Actions(move,current_node.x,current_node.y,current_node.orient, step_s, current_node.cost)  
             
             # Calculating the cost-to-go
             cost_to_go = dist((x, y), (goal.x, goal.y))  
             
-            new_node = Node(x,y,orient, cost,current_node, cost_to_go)   
-
-            new_node_id = key(new_node) 
-
+            new_node = SearchNode(x,y,orient, cost,current_node, cost_to_go)   
+            nnodeid = key(new_node) 
             if not avail_move(new_node.x, new_node.y, obs_space_array):
                 continue
-            elif new_node_id in nodes_explored:
+            elif nnodeid in old_nodes:
                 continue
-
-            # If unvisited node
-            if new_node_id in unexplored_nodes:
-                if new_node.cost < unexplored_nodes[new_node_id].cost: 
-                    unexplored_nodes[new_node_id].cost = new_node.cost
-                    unexplored_nodes[new_node_id].parent_id = new_node.parent_id
+            # If it is an unvisited node
+            if nnodeid in new_nodes:
+                if new_node.cost < new_nodes[nnodeid].cost: 
+                    new_nodes[nnodeid].cost = new_node.cost
+                    new_nodes[nnodeid].parent_id = new_node.parent_id
             else:
-                unexplored_nodes[new_node_id] = new_node
+                new_nodes[nnodeid] = new_node
             
-            heapq.heappush(priority_list, [(new_node.cost + new_node.cost_to_go), new_node]) 
+            heapq.heappush(p_list, [(new_node.cost + new_node.cost_to_go), new_node]) 
     return  all_nodes,0
 
 # backtracking the path and getting the "reversed" path
@@ -298,16 +286,16 @@ if __name__ == '__main__':
     total_clearance = int(total_clearance)
     
     # Taking Input of the desired radius of the robot
-    robot_radius = input("Please input the Robot Radius: ") 
-    robot_radius = int(robot_radius)
+    r_radius = input("Please input the Robot Radius: ") 
+    r_radius = int(r_radius)
     
     # Taking Input of the desired step size for A*
-    robot_step_size = input("Please input the Robot Step size(between 1 to 10): ")
-    robot_step_size = int(robot_step_size)
+    robot_step_s = input("Please input the Robot Step size(between 1 to 10): ")
+    robot_step_s = int(robot_step_s)
     
-    width = 1200
-    height = 500
-    obs_space_array = obstacle_space(width, height, total_clearance, robot_radius)
+    w = 1200
+    h = 500
+    obs_space_array = canvas_space(w, h, total_clearance, r_radius)
     cost_to_go = 0 # Cost-to-Go
     
     # Taking Input of the desired starting coordinates
@@ -350,9 +338,9 @@ if __name__ == '__main__':
     timer_start = time.time()
     
     # Initializing the start and goal node 
-    start_node = Node(s_x, s_y,s_t, 0.0, -1,cost_to_go)
-    goal_node = Node(g_x, g_y,g_t, 0.0, -1, cost_to_go)
-    all_nodes,flag = a_star_algorithm(start_node, goal_node, obs_space_array, robot_step_size)
+    start_node = SearchNode(s_x, s_y,s_t, 0.0, -1,cost_to_go)
+    goal_node = SearchNode(g_x, g_y,g_t, 0.0, -1, cost_to_go)
+    all_nodes,flag = a_star_algorithm(start_node, goal_node, obs_space_array, robot_step_s)
     
     # Calculating the optimal path from start to the goal node 
     if (flag) == 1:
